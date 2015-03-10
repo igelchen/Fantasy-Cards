e180e0d626add25a7535b3bd31b6a760faaca1cb
igelchen <killerpilze.funghi@web.de>
2015-03-10 20:22:56 +0100

    Control erweitert

--- Control.py
+++ Control.py
@@ -4,44 +4,19 @@ from GUI import *
 from Weapons import *
 from Network import *
 
-#orc = Orc(3,4,6)
-#orc.reduce_hp(50)
-#print orc.get_hp()
-#print orc.is_alive()
-#orc.reduce_hp(50)
-#print orc.get_hp()
-#print orc.is_alive()
-
-
-
-
 deck = Deck.create_random_deck(10)
-server = Connection()
-client = Connection("127.0.0.1")
-client.send_attacking_card(deck.next_card())
-action = server.get_action()
-print action.get_card()
-server.close()
-client.close()
 
+is_server = False
+if raw_input("als Server starten? (j/n):") == "j":
+    is_server = True
 
-gui = GUI()
-gui.show_deck(deck)
+if is_server:
+    connection = Connection()
+else:
+    connection = Connection("127.0.0.1")
 
-while True:
-    action = gui.get_action()
-
-    if action.get_type() == "ready":
-        print "Das Spiel beginnt"
-
-    elif action.get_type() == "swap":
-        deck.swap(action.get_index1(), action.get_index2())
-        gui.show_deck(deck)
-
-    elif action.get_type() == "attack":
-        print "Greife an mit %d" % action.get_weapon()
-
-    elif action.get_type() == "close":
-        print "Spiel beendet"
-        break
-
+if is_server:
+    action = connection.get_action()
+    if action.get_type() == "attack":
+        enemy_card = action.get_card()
+        enemy_card.attack()
